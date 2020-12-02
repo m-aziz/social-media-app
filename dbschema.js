d147e3be762f94056bb063e0f5769888b017e5f3
Muhammad Aziz <58401215+m-aziz@users.noreply.github.com>
2020-12-02 16:38:08 -0500

    Add files via upload

---
+++ dbschema.js
@@ -0,0 +1,23 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: 125,
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+  users: [
+    {
+      userId: "3904fj03j4gg03",
+      email: "new@email.com",
+      handle: "User560",
+      createdAt: "",
+      imageUrl: "image/sdfsfasdfafa",
+      bio: "Hello, I am a User",
+      website: '"https://user.com',
+      location: "Merica, Farmville",
+    },
+  ],
+};
